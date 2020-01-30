d0058cf744f1348e4879f00ae9623d23d580d690
Logan Murray <loganmurray16.95@gmail.com>
2020-01-29 22:45:59 -0600

    config files updated

---
+++ hw3-saved-files/testingHw3.java
@@ -0,0 +1,13 @@
+public class testingHw3 {
+
+  public static void main(String[] args) {
+    List<Integer> temp = new ArrayList<>();
+    temp.add(1);
+    temp.add(2);
+    temp.add(3);
+    temp.add(4);
+    System.out.println(temp);
+  }
+
+}
+
